e7104f1ec9c3c90d9e743fe425492506cfcd4a6e
Gaurav Sonawane <104220519+Gaur721@users.noreply.github.com>
2023-05-14 16:12:31 +0530

    Update Stock_analysis.py
    Adding team member names

--- Stock_analysis.py
+++ Stock_analysis.py
@@ -11,15 +11,15 @@ from imp_func import *
 
 # Add contact information
 st.sidebar.title("Gaurav Sonawane")
-st.sidebar.write("Machiine Learning professional")
-st.sidebar.write("You can reach me at:")
-st.sidebar.subheader("sonawanegauravd6@gmail.com")
-st.sidebar.subheader("[LinkedIn](https://www.linkedin.com/in/gaurav-sonawane-008342253)")
+st.sidebar.title("Simran Sah")
+st.sidebar.title("Sanskriti Joshi")
+st.sidebar.title("Deepraj Sonawane")
+st.sidebar.title("Suchet Suri")
 st.sidebar.subheader("[GitHub](https://github.com/Gaur721)")
 
 #Skills
 st.sidebar.header("Skills")
-st.sidebar.write("Here are some of my top skills:")
+st.sidebar.write("Here are some of our top skills:")
 st.sidebar.write("- Python programming")
 st.sidebar.write("- SQL")
 st.sidebar.write("- Github")
